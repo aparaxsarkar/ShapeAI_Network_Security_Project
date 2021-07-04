1a376b61a8af4e8a6501db2f50d8ae91ad9545dd
aparaxsarkar <81165861+aparaxsarkar@users.noreply.github.com>
2021-07-04 23:19:31 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,4 +1,8 @@
 import hashlib
+
+# asking for input string
 text = input("Enter string data: ")
+
+# using MD5 algorithm after converting to UTF-8
 md5_hash = hashlib.md5(text.encode()).hexdigest()
 print ("MD5: " + md5_hash)
