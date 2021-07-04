2870460f04fad2dc13fe12dcc031130533987b14
aparaxsarkar <81165861+aparaxsarkar@users.noreply.github.com>
2021-07-04 23:16:49 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,1 +1,16 @@
+import hashlib
 
+# asking for input string
+text = input("Enter string data: ")
+
+# using SHA-256 algorithm after converting to UTF-8
+sha256_hash = hashlib.sha256(text.encode()).hexdigest()
+print ("\nSHA-256: " + sha256_hash + "\n")
+
+# using SHA-3-256 algorithm after converting to UTF-8
+sha3_256_hash = hashlib.sha3_256(text.encode()).hexdigest()
+print ("SHA-3-256: " + sha3_256_hash + "\n")
+
+# using BLAKE2B algorithm after converting to UTF-8
+blake2b_hash = hashlib.blake2b(text.encode()).hexdigest()
+print ("BLAKE2B: " + blake2b_hash + "\n")
