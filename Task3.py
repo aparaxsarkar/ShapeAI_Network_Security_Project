32eeff1178d5d1bc9b998521bd44f05cda96779d
aparaxsarkar <81165861+aparaxsarkar@users.noreply.github.com>
2021-07-05 18:03:53 +0530

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,1 +1,10 @@
+import hashlib
+import os
 
+salt = os.urandom(32)
+password = input("Enter string data: ")
+
+key = hashlib.pbkdf2_hmac('sha256',password.encode(),salt,1000)
+# hash digest algorithm, UTF-8 form, salt, 1000 iterations
+
+print("\nDone salting with a 1000 iterations")
